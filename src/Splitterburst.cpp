a8a652876d695e912d7f830d23ba21a305e07b68
sheep <genericbean64@googlemail.com>
2021-05-10 19:45:30 +0100

    continued splitterburst development

--- src/Splitterburst.cpp
+++ src/Splitterburst.cpp
@@ -4,10 +4,14 @@
 struct Splitterburst : Module {
 	enum ParamIds {
 		STEPS_PARAM,
+		STEPS_ATT,
+		MULTI_ATT,
 		MULTI_PARAM,
 		NUM_PARAMS
 	};
 	enum InputIds {
+		MULTI_CV,
+		STEPS_CV,
 		GATE_INPUT,
 		TOG_INPUT,
 		OFF_INPUT,
@@ -17,6 +21,7 @@ struct Splitterburst : Module {
 		NUM_INPUTS
 	};
 	enum OutputIds {
+		STEP_OUTPUT,
 		PLAIN_OUTPUT,
 		GATE_OUTPUT,
 		SOLO_OUTPUT,
@@ -33,6 +38,8 @@ struct Splitterburst : Module {
 		
 		configParam(STEPS_PARAM, 1.f, 64.f, 4.f, "");
 		configParam(MULTI_PARAM, 1.f, 64.f, 2.f, "");
+		configParam(MULTI_ATT, -1.f, 1.f, 0.f, "");
+		configParam(STEPS_ATT, -1.f, 1.f, 0.f, "");
 	}
 
 	dsp::PulseGenerator plainPulse;	
@@ -48,7 +55,7 @@ struct Splitterburst : Module {
 	float timer=0;
 	float wait=0;
 	float goal=0;
-	int step = 0;
+	float step = 0;
 	bool trigon = false;
 	bool gateon = false;
 
@@ -75,8 +82,8 @@ struct Splitterburst : Module {
 	
 	bool on = (gateon||trigon);
 	
-	int steps = round(clamp(params[STEPS_PARAM].getValue(),1.f,64.f));
-	int multiply = round(clamp(params[MULTI_PARAM].getValue(),1.f,64.f));
+	float steps = round(clamp(params[STEPS_PARAM].getValue()+inputs[STEPS_CV].getVoltage()*6.4*params[STEPS_ATT].getValue(),1.f,64.f));
+	int multiply = round(clamp(params[MULTI_PARAM].getValue()+inputs[MULTI_CV].getVoltage()*6.4*params[MULTI_ATT].getValue(),1.f,64.f));
 	
 	wait++;
 	timer++;
@@ -98,8 +105,10 @@ struct Splitterburst : Module {
 	}
 	
 	if(stepTrigger.process(inputs[STEP_INPUT].getVoltage())){
-		step++;
-		if(step>=steps){step=0;trigon=false;}
+		if(trigon){
+			step++;
+			if(step>=steps){step=0;trigon=false;}
+		}
 	}
 	
 
@@ -110,6 +119,7 @@ struct Splitterburst : Module {
 	outputs[PLAIN_OUTPUT].setVoltage((plainPulse.process(args.sampleTime) ? 10.0f : 0.0f));
 	outputs[SOLO_OUTPUT].setVoltage((soloPulse.process(args.sampleTime) ? 10.0f : 0.0f));
 	outputs[CLOCK_OUTPUT].setVoltage((clockPulse.process(args.sampleTime) ? 10.0f : 0.0f));
+	outputs[STEP_OUTPUT].setVoltage(step*10/steps);
 	
 	
 	}
@@ -126,22 +136,29 @@ struct SplitterburstWidget : ModuleWidget {
 		addChild(createWidget<ScrewSilver>(Vec(RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
 		addChild(createWidget<ScrewSilver>(Vec(box.size.x - 2 * RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
 		
-		addChild(createLight<MediumLight<GreenLight>>(mm2px(Vec(16, 35)), module, Splitterburst::ON_LIGHT));
+		addChild(createLight<MediumLight<GreenLight>>(mm2px(Vec(16, 20)), module, Splitterburst::ON_LIGHT));
 
 
 		
 		addParam(createParamCentered<RoundBlackSnapKnob>(mm2px(Vec(12, 100)), module, Splitterburst::STEPS_PARAM));
 		addParam(createParamCentered<RoundBlackSnapKnob>(mm2px(Vec(24, 100)), module, Splitterburst::MULTI_PARAM));
+		addParam(createParamCentered<RoundSmallBlackKnob>(mm2px(Vec(24, 90)), module, Splitterburst::MULTI_ATT));
+		addParam(createParamCentered<RoundSmallBlackKnob>(mm2px(Vec(12, 90)), module, Splitterburst::STEPS_ATT));
+		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(24, 80)), module, Splitterburst::MULTI_CV));
+		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(12, 80)), module, Splitterburst::STEPS_CV));
 		
 		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(8, 35)), module, Splitterburst::CLOCK_INPUT));
 		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(8, 50)), module, Splitterburst::TRIG_INPUT));
 		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(18, 50)), module, Splitterburst::OFF_INPUT));
 		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(28, 50)), module, Splitterburst::TOG_INPUT));
 		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(8, 64)), module, Splitterburst::STEP_INPUT));
-		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(18, 64)), module, Splitterburst::GATE_INPUT));
+		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(18, 35)), module, Splitterburst::GATE_INPUT));
+		
+		
 		
 		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(28, 35)), module, Splitterburst::PLAIN_OUTPUT));
 		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(28, 64)), module, Splitterburst::GATE_OUTPUT));
+		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(18, 64)), module, Splitterburst::STEP_OUTPUT));
 
 		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(12, 116)), module, Splitterburst::SOLO_OUTPUT));		
 		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(24, 116)), module, Splitterburst::CLOCK_OUTPUT));
