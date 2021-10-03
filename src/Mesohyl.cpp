03fcf9d700502899d5382886eefbf6f50958bee4
EaterOfSheep <genericbean64@googlemail.com>
2021-10-03 10:45:38 +0100

    Mesohyl complete

--- src/Mesohyl.cpp
+++ src/Mesohyl.cpp
@@ -7,37 +7,100 @@ struct Mesohyl : Module {
 //And and Or modes for Global SandH
 //Lights for un-updated knobs
 //Unupdated-count output (+1V for each unupdated knob)
+//Range knob, bipolar switch.
 
 
 	enum ParamIds {
+		ANDOR_PARAM,
+		RANGE_PARAM,
+		BIPOLAR_PARAM,
 		KNOB_PARAM,
-		NUM_PARAMS=KNOB_PARAM+5
+		NUM_PARAMS=KNOB_PARAM+6
 	};
 	enum InputIds {
 		GSANDH_INPUT,
 		KSANDH_INPUT,
-		NUM_INPUTS=KSANDH_INPUT+5
+		NUM_INPUTS=KSANDH_INPUT+6
 	};
 	enum OutputIds {
 		KOUT_OUTPUT,
-		NUM_OUTPUTS=KOUT_OUTPUT+5
+		NUM_OUTPUTS=KOUT_OUTPUT+6
 	};
 	enum LightIds {
 		CHANGE_LIGHT,
-		NUM_LIGHTS=CHANGE_LIGHT+5
+		NUM_LIGHTS=CHANGE_LIGHT+6
 	};
 
-
-	dsp::SchmittTrigger globalTrigger;
-	dsp::SchmittTrigger knobTrigger[5];
+	float output[6] = { 0.f };
 
 	Mesohyl() {
 
 		config(NUM_PARAMS, NUM_INPUTS, NUM_OUTPUTS, NUM_LIGHTS);
 
+		configParam(RANGE_PARAM, 0.f, 10.f, 1.f, "");
+
+		for(int i = 0; i<6; i++){
+			configParam(KNOB_PARAM+i, 0.f, 1.f, 0.5, "");
+		}
+
 	}
 
 	void process(const ProcessArgs& args) override {
+
+		for(int i = 0; i<6; i++){
+
+			float knob = params[KNOB_PARAM+i].getValue();
+			float range = params[RANGE_PARAM].getValue();
+
+			if(params[BIPOLAR_PARAM].getValue()){
+				knob = (knob-0.5)*2.f;
+			}
+
+			knob = knob*range;
+
+
+			if(isUpdating(i)){
+				output[i] = knob;
+			}
+			outputs[KOUT_OUTPUT + i].setVoltage(output[i]);
+			lights[CHANGE_LIGHT+i].setBrightness(output[i]!=knob);
+		}
+
+	}
+
+	bool isUpdating(int i){
+
+		bool gtrue = false;
+		bool strue = false;
+
+		bool gplug = true;
+		bool splug = true;
+
+		if(!inputs[GSANDH_INPUT].isConnected()){gtrue=true; gplug=false;}else{gtrue = inputs[GSANDH_INPUT].getVoltage()>=1;}
+		if(!inputs[KSANDH_INPUT+i].isConnected()){strue=true; splug=false;}else{strue = inputs[KSANDH_INPUT+i].getVoltage()>=1;}
+
+		if(params[ANDOR_PARAM].getValue()){ //AND MODE
+
+			return (gtrue&&strue);
+
+
+		}else{ //OR MODE
+
+			if(!splug && !gplug){
+				return true;
+			}
+
+			if(splug && !gplug){
+				return strue;
+			}
+
+			if(!splug && gplug){
+				return gtrue;
+			}
+
+			return (gtrue||strue);
+
+		}
 
 	}
 
@@ -54,13 +117,16 @@ struct MesohylWidget : ModuleWidget {
 		addChild(createWidget<XtrtnScrew>(Vec(RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
 		addChild(createWidget<XtrtnScrew>(Vec(box.size.x - 2 * RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
 
-		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(20, 22)), module, Mesohyl::GSANDH_INPUT));
+		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(15, 22)), module, Mesohyl::GSANDH_INPUT));
+
+		addParam(createParamCentered<CKSS>(mm2px(Vec(25, 22)), module, Mesohyl::ANDOR_PARAM));
+		addParam(createParamCentered<CKSS>(mm2px(Vec(27, 115)), module, Mesohyl::BIPOLAR_PARAM));
+		addParam(createParamCentered<XtrtnKnob>(mm2px(Vec(18,115)), module, Mesohyl::RANGE_PARAM));
 
 
 		int ygap = 12;
 		int ystart = 42;
-		for(int i = 0; i<5; i++){
-
+		for(int i = 0; i<6; i++){
 
 			addChild(createLightCentered<SmallLight<XtrtnPinkLight>>(mm2px(Vec(14, ystart+i*ygap-4)), module, Mesohyl::CHANGE_LIGHT+i));
 
